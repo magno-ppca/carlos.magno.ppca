cd23099d048f618d3d87b199f91702cf0d61cb92
carlos magno <magnotrue@gmail.com>
2016-06-12 23:05:26 -0300

    retirado flag sujeira do codigo

--- app/javascripts/app.js
+++ app/javascripts/app.js
@@ -12,7 +12,6 @@ function refreshBalance() {
 
   meta.getBalance.call(account, {from: account}).then(function(value) {
     var balance_element = document.getElementById("balance");
-<<<<<<< HEAD
     //balance_element.innerHTML = value.valueOf();
     var defaultBlock =  web3.eth.coinbase;
    document.getElementById("ddd").innerHTML = defaultBlock;
@@ -31,12 +30,6 @@ function refreshBalance() {
   }).catch(function(e) {
     console.log(e);
     setStatus("Error getting balance; veja o log.");
-=======
-    balance_element.innerHTML = value.valueOf();
-  }).catch(function(e) {
-    console.log(e);
-    setStatus("Error getting balance; see log.");
->>>>>>> a532f0e9812da91f09e32c674b894273eb2abce8
   });
 };
 
@@ -49,39 +42,23 @@ function sendCoin() {
   setStatus("Initiating transaction... (please wait)");
 
   meta.sendCoin(receiver, amount, {from: account}).then(function() {
-<<<<<<< HEAD
     setStatus("Transação Terminada, CRC Validado!");
     refreshBalance();
   }).catch(function(e) {
     console.log(e);
     setStatus("Erro veja o log.");
-=======
-    setStatus("Transaction complete!");
-    refreshBalance();
-  }).catch(function(e) {
-    console.log(e);
-    setStatus("Error sending coin; see log.");
->>>>>>> a532f0e9812da91f09e32c674b894273eb2abce8
   });
 };
 
 window.onload = function() {
   web3.eth.getAccounts(function(err, accs) {
     if (err != null) {
-<<<<<<< HEAD
       alert("Existe um erro na conta blockchain.");
-=======
-      alert("There was an error fetching your accounts.");
->>>>>>> a532f0e9812da91f09e32c674b894273eb2abce8
       return;
     }
 
     if (accs.length == 0) {
-<<<<<<< HEAD
       alert("Erro cliente ethereum, veja o testrpc esta Iniciado.");
-=======
-      alert("Couldn't get any accounts! Make sure your Ethereum client is configured correctly.");
->>>>>>> a532f0e9812da91f09e32c674b894273eb2abce8
       return;
     }
 
@@ -90,8 +67,4 @@ window.onload = function() {
 
     refreshBalance();
   });
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> a532f0e9812da91f09e32c674b894273eb2abce8
